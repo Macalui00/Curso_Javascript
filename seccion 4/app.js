1ef33344d593ec900753ac444d43cfb301838e24
Macarena Lui <39487444+Macalui00@users.noreply.github.com>
2020-08-03 19:19:06 -0300

    Mejorando siempre

--- seccion 4/app.js
+++ seccion 4/app.js
@@ -17,10 +17,12 @@ Y probablemente la cosa más importante de todo es el puntaje de cada jugador.
 */
 //var score1, scored2 = 0;
 
-var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0; //0 el primer jugador, 1 el segundo jugador.
+var scores, roundScore, activePlayer, gamePlaying;
+// scores = [0,0];
+// roundScore = 0;
+// activePlayer = 0; //0 el primer jugador, 1 el segundo jugador.
+
+init();
 
 //ahora creamos el dado, para eso necesitamos calcular un numero al azar.
 /*
@@ -87,7 +89,7 @@ console.log(x);*/
 //Otra cosa que queremos hacer es esconder el dado al principio cuando abrimos el juego.
 //en este caso no tiene un id, pero si una clase por eso empieza con '.' y no con '#'.
 //Ademas de esto, queremos cambiar el estilo de la imagen del dado, por lo cual, usamos el metodo style y luego seleccionamos el estilo que queremos que cambiar:
-document.querySelector('.dice').style.display = 'none';
+// document.querySelector('.dice').style.display = 'none';
 //display = none, en css nos permite ocultar un elemento.
 
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -113,31 +115,149 @@ btn();
 document.querySelector('.btn-roll').addEventListener('click', btn);
 */
 
-document.getElementsById('score-0').textContent = '0';
-document.getElementsById('score-1').textContent = '0';
-document.getElementsById('current-0').textContent = '0';
-document.getElementsById('current-1').textContent = '0';
+    document.getElementsById('score-0').textContent = '0';
+    document.getElementsById('score-1').textContent = '0';
+    document.getElementsById('current-0').textContent = '0';
+    document.getElementsById('current-1').textContent = '0';
 
 /* Pero tambien puedo hacer lo siguiente: */
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    //¿Que sucedera entonces ni bien cliqueen el boton:
-    /* 1. el numero random, por lo que tomaremos la funcion que estaba arriba
-    */
-   var dice = Math.floor(Math.random()*6)+1;
+    if (gamePlaying){
+        //¿Que sucedera entonces ni bien cliqueen el boton:
+        /* 1. el numero random, por lo que tomaremos la funcion que estaba arriba
+        */
+        var dice = Math.floor(Math.random()*6)+1;
 
-   /* 2. Mostrar el resultado:  lo primero que tenemos que hacer es que vuelva a aparecer el dado ya que lo habiamos ocultado*/
-   var diceDOM = document.querySelector('.dice');
-   diceDOM.style.display = 'block';
-   diceDOM.src = 'dice-'+ dice + '.png';
+        /* 2. Mostrar el resultado:  lo primero que tenemos que hacer es que vuelva a aparecer el dado ya que lo habiamos ocultado*/
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-'+ dice + '.png';
 
-   /* 3. actualizar el round score solamente si el numero rolleado no es un 1 */
+        /* 3. actualizar el round score solamente si el numero rolleado no es un 1 */
+        if (dice > 1){
+            //add score
+            roundScore += dice; //Actualizamos el puntaje
+            document.querySelector('#current-' + activePlayer).textContent = roundScore; //Lo mostramos por pantalla.
+        } else { //Cuando el dado es igual a 1, es el turno del jugador 1
+            //next player
+            nextPlayer();
+        }   
+    }
     
+    
+
+
 });
 //FUNCION ANONIMA que no tiene nombre y por eso no podemos usar afuera de este contexto.
 //Como esta funcion no la queremos usar en otro lado ademas de este caso particular, es ideal que la definamos de esta manera.
 
 /*-------------------------------------------------------------------------------------------------------------------------------------- 
-Leccion 3: Updating scores and changing th actibe player
+Lección 50: Updating scores and changing th actibe player
 
 En esta leccion podremos descubrir que es el operador ternario y aprenderemos como agregar, eliminar o alterar html clases.
+
+(se completo el 3er paso de la función.)
+*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------- 
+Leccion 51: Implementing Our 'Hold? Function and the DRY Principle
+
+En esta leccion vamos a implementar nuestra funcion de retencion de puntaje (guardar puntaje acumulado) y vamos a ver un poco
+del principio de NO TE REPITAS A VOS MISMO.
+
+POR LO CUAL OTRA VEZ VAMOS A USAR UN EVENT LISTENER, Y EN ESTE CASO VA A SER PARA EL OTRO BOTON: HOLD. BUSCAMOS COMO SE LLAMA LA CLASE: "btn-hold"
+*/
+
+document.querySelector('btn-hold').addEventListener('click',function(){
+    if (gamePlaying){
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore; //actualizamos el puntaje acumulado
+        
+        //Update the UI 
+        document.querySelector('#score-' + activePlayer).textContent = scores [activePlayer]; //mostramos el nuevo puntaje acumulado por pantalla
+        //el tema es que cuando lo actualizamos, se nos tiene que resetear el puntaje actual y tiene que pasar al siguiente jugador
+
+        //Check if player won the game
+        if (scores[activePlayer] >= 20){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            //Para que el punto rojo desaparezca y que el ganador aparezca en letra roja:
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); 
+            //Hay otro problema que tambien tenemos que corregir que es que si ya gano, podemos seguir presionando roll de dice y acumular puntaje...
+            //ponemos el gameplaying en false
+            gamePlaying = false;
+        } else{
+            //next player
+            nextPlayer();
+        }  
+    }
+
+});
+
+
+//creamos una nueva funcion para pasar al nuevo jugador para justamente no romper el principio de no repetirse a sí mismo:
+function nextPlayer(){
+ //next player: usando el operador ternario
+ activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+ roundScore = 0; //se reseteea su contador a cero.
+
+ //Hay que mostrar tambien que su contador se puso en cero en la interfaz de usuario.
+ document.getElementsById('current_0').textContent = '0';
+ document.getElementsById('current_1').textContent = '0';
+
+ //Ahora lo que queremos hacer es cambiar lo que se ve en la intefaz, donde aparece el punto rojo y que el cuadro gris ahora tiene que ser el otro jugador.
+ //document.querySelector('.player-0-panel').classList.remove('active'); //Seleccionamos con el Query selector la clase, y luego removemos de la lista de clases el active.
+ //document.querySelector('.player-1-panel').classList.add('active'); //Seleccionamos con el Query selector la clase, y luego agregamos a la lista de clases el active.
+ 
+//Ahora podemos ver de manera VISIBLE que el usuario 2 es el que esta activo.
+ //Lo que si no cambio es el color del jugador "jugador 2": para lo cual hay una mejor forma de hacer lo anterior.
+
+ document.querySelector('.player-0-panel').classList.toggle('active'); //Toggle me permite alterar una clase, si existe la quita, si no existe la crea
+ document.querySelector('.player-1-panel').classList.toggle('active'); //Toggle me permite alterar una clase, si existe la quita, si no existe la crea
+
+ //Otra cosa que queremos es que cuando se cambie de jugador, el dado no este visible:
+ document.querySelector('.dice').style.display = 'none';
+
+}
+
+
+/* Lección 52: Creating a Game Initialization Function
+Entonces ahora lo que haremos es implementar el botón de New Game
+En este caso el nombre de la clase del boton es: 'btn-new'
+*/
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+//Funcion para inicializar las variables
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementsById('score-0').textContent = '0';
+    document.getElementsById('score-1').textContent = '0';
+    document.getElementsById('current-0').textContent = '0';
+    document.getElementsById('current-1').textContent = '0';
+    document.getElementsById('name-0').textContent = 'Player 1';
+    document.getElementsById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
+
+
+/*Leccion 53: Toques finales: una variable de estado
+Que es una variable de estado? Simplemente nos dice la condicion de un sistema.
+Esa es la definicion. Necesitamos una variable de estado cuando necesitamos recordar algo.
+o necesitamos el estado de algo.
+
+En este caso la variable será si nuestro juego se jugará o no se jugará
+
+
 */
